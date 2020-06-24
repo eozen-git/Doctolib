9cf7edd3647c95cd332795d92b7baf3d9d90f979
DIERICK Dewi <dierickd@outlook.fr>
2020-06-24 15:27:09 +0200

    first commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,19 @@
 namespace App\Controller;
 
 
-class HomeController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
 {
+    /**
+     * @Route("/", name="app_index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('home/home.html.twig');
+    }
 
 }
