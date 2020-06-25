8a3b073deea63a42cedf76e8791d4fd4d803d238
Julien <eoz.wild@gmail.com>
2020-06-25 23:41:08 +0200

    Ajout DrugController

--- src/Controller/DiseaseController.php
+++ src/Controller/DiseaseController.php
@@ -5,20 +5,12 @@ namespace App\Controller;
 use App\Entity\Conversation;
 use App\Entity\Message;
 use App\Form\MessageType;
-use App\Repository\ConversationRepository;
 use App\Repository\DiseaseRepository;
 use App\Repository\DrugRepository;
 use App\Repository\MoleculeRepository;
-use App\Service\OnboardingConversation;
-use BotMan\BotMan\BotMan;
-use BotMan\BotMan\BotManFactory;
-use BotMan\BotMan\Cache\SymfonyCache;
-use BotMan\BotMan\Drivers\DriverManager;
-use BotMan\Drivers\Facebook\FacebookDriver;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -38,17 +30,9 @@ class DiseaseController extends AbstractController
     public function index(
         Request $request,
         EntityManagerInterface $entityManager,
-        MoleculeRepository $moleculeRepository,
-        DrugRepository $drugRepository,
         DiseaseRepository $diseaseRepository
     ): Response
     {
-        $conversation = new Conversation();
-        $message = new Message();
-        $diseases = $diseaseRepository->findAll();
-        $form = $this->createForm(MessageType::class, $message);
-        $form->handleRequest($request);
-        $data = $form->getData();
         $diseases = $diseaseRepository->findAll();
         $illness = [];
         foreach ($diseases as $disease) {
---
+++ src/Controller/DrugController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conversation;
+use App\Entity\Message;
+use App\Form\MessageType;
+use App\Repository\DiseaseRepository;
+use App\Repository\DrugRepository;
+use App\Repository\MoleculeRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DrugController extends AbstractController
+{
+    /**
+     * @Route("/drugs/index", name="drugs_index")
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param MoleculeRepository $moleculeRepository
+     * @param DrugRepository $drugRepository
+     * @param DiseaseRepository $diseaseRepository
+     * @return Response
+     */
+        public function index(
+            Request $request,
+            EntityManagerInterface $entityManager,
+            MoleculeRepository $moleculeRepository,
+            DrugRepository $drugRepository,
+            DiseaseRepository $diseaseRepository
+        ): Response
+        {
+            $drugs = $drugRepository->findAll();
+            $medicines = [];
+            foreach ($drugs as $drug) {
+                $medicines[] = $drug->getName();
+            }
+            $conversation = new Conversation();
+            $medicines = implode(", ", $medicines);
+            $conversation->setMessage("The available medicines are " . $medicines . '.');
+            $conversation->setPostAt(new DateTime());
+            $entityManager->persist($conversation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('botman_chat');
+        }
+}
