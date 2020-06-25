50aa35748dbf356178a4a127f090edb8d241cf65
Julien Gault <jugault45@gmail.com>
2020-06-25 12:14:04 +0200

    entity conversation

--- src/Controller/BotmanController.php
+++ src/Controller/BotmanController.php
@@ -3,14 +3,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Conversation;
 use App\Entity\Message;
 use App\Form\MessageType;
+use App\Repository\ConversationRepository;
 use App\Service\OnboardingConversation;
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Cache\SymfonyCache;
 use BotMan\BotMan\Drivers\DriverManager;
 use BotMan\Drivers\Facebook\FacebookDriver;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,23 +35,30 @@ class BotmanController extends AbstractController
 
     /**
      * @Route("/botman/chat", name="botman_chat")
+     * @param Request $request
+     * @param ConversationRepository $conversationRepository
+     * @param EntityManagerInterface $entityManager
      * @return Response
      */
-    public function chat(Request $request): Response
+    public function chat(Request $request, ConversationRepository $conversationRepository, EntityManagerInterface $entityManager): Response
     {
-        $conversation = [];
+        $conversation = new Conversation();
+        $conversations = $conversationRepository->findAll();
         $message = new Message();
         $form = $this->createForm(MessageType::class,$message);
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted()) {
             $data = $form->getData();
+
             $conversation = $data['message'];
+            $entityManager->persist($conversation);
+            $entityManager->flush();
             if ($data['message'] === 'hello') {
                 $conversation = 'Salut Doc';
             }
         }
         return $this->render('home/chat.html.twig', [
             'form' => $form->createView(),
-            'conversation' => $conversation
+            'conversation' => $conversations
         ]);
     }
 }
